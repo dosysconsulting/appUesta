4521793328f72fc10e4434737c8881cd2c6ab314
dosysconsulting <resanchez@dosys.com.co>
2022-11-24 19:17:22 -0500

    test
    subida del test

--- Proyecto_Ricardo/apuestas/src/test/java/com/app/web/ApuestasApplicationTests.java
+++ Proyecto_Ricardo/apuestas/src/test/java/com/app/web/ApuestasApplicationTests.java
@@ -9,5 +9,33 @@ class ApuestasApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	  public ClienteTest() {
 
+	        apuesta = new Apuesta();
+	    }
+	    @Test
+	    public void pruebaBD() throws ClassNotFoundException{
+	     
+	    }
+	    //@Test
+	    //public void registrarNull(){
+	    //    boolean rta = apuesta.RegistroCliente(null);
+	    //    assertFalse(rta);
+	    //}
+	    
+	    //@Test
+	    //public void registarClienteSinContrasena(){
+	    //    boolean rta = true;
+	    //    Cliente cliente = new Cliente();
+	    //    cliente.setUsuario("pepe");
+	    //    //cliente.setContrasena("XXXXX");
+	   //     cliente.setCorreo("pepe@gmail.com");
+	   //     cliente.setCelular("3208528838");
+	   //     cliente.setCuenta("0009899");
+	    //    cliente.setTipoPerfil("C");
+	    //    if (cliente.getContrasena() == null) {
+	    //        rta = false;
+	    //    }    
+	    //    assertEquals(rta,false);
+	    //}
 }
